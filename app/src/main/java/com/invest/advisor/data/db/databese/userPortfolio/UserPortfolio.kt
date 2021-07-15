7c4e5a120b70333509049c05871295d86fb4e6ce
GorAnatoS <qsufff@gmail.com>
2021-07-15 23:25:13 +0300

    added description

--- app/src/main/java/com/invest/advisor/data/db/databese/userPortfolio/UserPortfolio.kt
+++ app/src/main/java/com/invest/advisor/data/db/databese/userPortfolio/UserPortfolio.kt
@@ -1,17 +1,19 @@
-package com.invest.advisor.data.db.userPortfolio
+package com.invest.advisor.data.db.databese.userPortfolio
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.invest.advisor.data.db.entity.USER_PORTFOLIO_DATABASE_NAME
+import com.invest.advisor.data.db.entity.USER_PORTFOLIO_DATABASE_VERSION
+import com.invest.advisor.data.db.entity.UserPortfolioEntry
 
 
 /**
- * Created by qsufff on 9/13/2020.
+ * Database that contains stocks added by user.
  */
 
-
-@Database(entities = [UserPortfolioEntry::class], version = DATABASE_VERSION, exportSchema = false)
+@Database(entities = [UserPortfolioEntry::class], version = USER_PORTFOLIO_DATABASE_VERSION, exportSchema = false)
 abstract class UserPortfolio : RoomDatabase() {
 
     abstract val userPortfolioDao: UserPortfolioDao
@@ -31,7 +33,7 @@ abstract class UserPortfolio : RoomDatabase() {
                     instance = Room.databaseBuilder(
                         context.applicationContext,
                         UserPortfolio::class.java,
-                        DATABASE_NAME
+                        USER_PORTFOLIO_DATABASE_NAME
                     ).build()
                 }
 
