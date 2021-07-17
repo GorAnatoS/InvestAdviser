28056139826f3c925a4f9a3d2423f0ee9cc3e0f8
GorAnatoS <qsufff@gmail.com>
2021-07-18 01:51:10 +0300

    added obj with common application constants

---
+++ app/src/main/java/com/invest/advisor/internal/Constants.kt
@@ -0,0 +1,10 @@
+package com.invest.advisor.internal
+
+
+/**
+ * Created by qsufff on 7/18/2021.
+ */
+object Constants {
+    const val ARG_PARAM_SECID = "secId"
+    const val ARG_PARAM_SECPRICE = "secPrice"
+}
