6d232dcd3b3d3ca58670ec7ff9289163562b454c
GorAnatoS <qsufff@gmail.com>
2021-07-20 22:14:14 +0300

    navController fixed

--- app/src/main/java/com/invest/advisor/ui/MainActivity.kt
+++ app/src/main/java/com/invest/advisor/ui/MainActivity.kt
@@ -2,13 +2,16 @@ package com.invest.advisor.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.invest.advisor.R
+
 
 /**
  * MainActivity class where we setting up navigationController and bottomNavigationBar
@@ -29,10 +32,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController = Navigation.findNavController(
-            this,
-            R.id.nav_host_fragment
-        )
+        navController = getNavController()
 
         val bottomNav: BottomNavigationView = findViewById(R.id.nav_view)
         bottomNav.setupWithNavController(navController)
@@ -42,5 +42,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
+    }
+
+    private fun getNavController(): NavController {
+        val fragment: Fragment? = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        check(fragment is NavHostFragment) {
+            ("Activity " + this
+                    + " does not have a NavHostFragment")
+        }
+        return (fragment as NavHostFragment?)!!.navController
     }
 }
